f979d6c126849f74cac22207162e09edf159cabc
Sanjay Shiwakoti <sanjayshiwakoti@lftechnology.com>
2018-02-04 13:46:36 +0545

    Fix the base url.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import { Router } from 'express';
 import HttpStatus from 'http-status-codes';
+import * as userServices from '../services/userServices';
 
 const router = Router();
 
 /**
  * GET /api/users
  */
-router.get('/', (req, res, next) => {
-  console.log(process.env.AUTH_BASE_PATH + 'users');
-   axios.get(process.env.AUTH_BASE_PATH + 'users')
+router.get('/:id', (req, res, next) => {
+  
+  userServices.getUser(req.params.id)
   .then(function (response) {
     res.json(response.data);
   })
